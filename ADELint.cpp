77b5bbbea2ec5336cb671745fb883eb1d1899453
Dorian Ivancic <dorian.ivancic@gmail.com>
2016-01-22 00:03:09 +0100

    Add ADELint constructors

--- ADELint.cpp
+++ ADELint.cpp
@@ -5,7 +5,7 @@
 namespace dlib {
 
 /**
- * Default constructor: constructs 0.
+ * Default constructor: constructs 0
  */
 ADELint::ADELint() : SuperBin() {}
 
@@ -16,6 +16,29 @@ ADELint::ADELint() : SuperBin() {}
 ADELint::ADELint(std::string number, int base, int signum) {
   SuperBin(number, base,
     (signum < 0) ? SuperBin::Sign::NEG : SuperBin::Sign::POS);
+}
+
+/**
+ * Constructor which constructs a number passed by as a pointer to a C-string,
+ * in a given base and with a given signum (neagtive if signum < 0, positive
+ * if signum >= 0)
+ */
+ADELint::ADELint(const char *number, int base, int signum) :
+  ADELint(std::string(number), base, signum) {}
+
+/**
+ * Copy constructor
+ */
+ADELint::ADELint(const ADELint &src) : SuperBin(src) {}
+
+/**
+ * Copy assignment operator
+ */
+ADELint&
+ADELint::operator=(const ADELint &rhs) {
+  SuperBin::operator=(rhs);
+
+  return *this;
 }
 
 }  // namespace dlib
